baabe5b828aedf313e6580b079f80e52ebac37a9
snovakovic <stefan.novakovich@gmail.com>
2019-12-10 14:26:14 +0100

    Update integration tests

---
+++ test/integration/dist.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+const sortFull = require('../../dist/sort');
+const sortMin = require('../../dist/sort.min');
+
+// Just sanity checks to ensure dist is not broken
+// For more detail unit tests check sort.spec.js
+function runTests(sort) {
+  assert.deepEqual(sort([1, 4, 2]).asc(), [1, 2, 4]);
+  assert.deepEqual(sort([1, 4, 2]).by({ asc: true }), [1, 2, 4]);
+  assert.deepEqual(sort([1, 4, 2]).desc(), [4, 2, 1]);
+  assert.deepEqual(sort([1, 4, 2]).by({ desc: true }), [4, 2, 1]);
+}
+
+runTests(sortFull);
+runTests(sortMin);
